2ac34396d0788dd4674db7d3fd6cee4667e325a0
Cameron Glanville <cdglanville@gmail.com>
2020-08-24 15:25:41 +0200

    printing out args

--- src/com/avaj/Main.java
+++ src/com/avaj/Main.java
@@ -3,7 +3,12 @@ package com.avaj;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("testing");
+        System.out.println("Hello World");
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println(args[i]);
+            }
+        }
     }
 
 }
