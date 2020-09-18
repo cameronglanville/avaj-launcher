78be1ce83ef8eed4d66f146d5bd8f6566c6cc287
Cameron Glanville <cdglanville@gmail.com>
2020-09-18 17:52:32 +0200

    Main is opening input

--- avaj/launcher/Main.java
+++ avaj/launcher/Main.java
@@ -1,13 +1,33 @@
 package avaj.launcher;
 
+import java.io.FileReader;
+// import java.io.BufferedReader;
+// import java.io.File;
+// import java.io.FileWriter;
+// import java.io.BufferedWriter;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
         // if (args.length > 0) {
         // for (int i = 0; i < args.length; i++) {
         // System.out.println(args[i]);
         // }
         // }
+        if (args.length == 0) {
+            throw new Exception("please pass avaj launcher a scenario");
+        } else if (args.length > 1) {
+            throw new Exception("too many arguments");
+        }
+
+        //System.out.println(args[0]);
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader("./" + args[0]);
+        } catch(Exception e) {
+            throw new Exception("bad input");
+        }
+        
+        fileReader.close();
     }
 }
